e80c6ac2e3500199404045be8b991300ab2fbdad
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-02-10 01:21:13 +0530

    Syntax and use of useEffect hook

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import CoinTable from '../components/CoinTable/CoinTable';
+import './App.css'
+
+function App() {
+    return(
+       <>
+        <h1>Hello</h1>
+
+        <CoinTable />
+        </>
+    )
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const { default: themes } = require('daisyui/theme/object');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui')
+  ],
+  daisyui: {
+  themes: ["forest"],
+},
+}
