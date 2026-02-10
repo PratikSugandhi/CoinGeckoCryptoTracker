0d85138fed41f83d7334921470596700218f2edc
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-02-11 01:24:05 +0530

    Added the UI Logic of CoinTable

--- src/services/fetchCoinData.js
+++ src/services/fetchCoinData.js
@@ -4,8 +4,8 @@ export async function fetchCoinData(page=1,currency='usd') {
     const perPage=10;
     try {
         const response=await axiosInstance.get(`/coins/markets?vs_currency=${currency}&order=market_cap_desc&perPage=${perPage}&page=${page}`);
-        console.log(response);
-        return response;
+        
+        return response.data;
         
     } catch (error) {
         console.error(error);
