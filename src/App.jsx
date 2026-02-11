9e91952afb67112b115d30fe39bc97406df6d8ad
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-02-12 01:00:28 +0530

    Added the changed on  mechanims of zustand a state management tool

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import CoinTable from './components/CoinTable/CoinTable.jsx'
 import './App.css'
 import Banner from './components/Banner/Banner.jsx';
 import Navbar from "./components/Navbar/Navbar.jsx"
-import { useState } from 'react';
+// import { useState } from 'react';
 // import { CurrencyContext } from './context/CurrencyContext.js';
 import Home from './pages/Home.jsx';
 function App() {
-      const [currency, setCurrency] = useState('usd')
+    //   const [currency, setCurrency] = useState('usd')
     return(
        <>
            {/* <CurrencyContext.Provider value={ {currency, setCurrency} }>
