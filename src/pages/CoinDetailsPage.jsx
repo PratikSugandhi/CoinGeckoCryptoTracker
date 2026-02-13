d3014ca6ee2a443f0385a9aba2d21ea38a4c6fa6
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-02-13 08:59:11 +0530

    Added readme for useParams

--- src/pages/CoinDetailsPage.jsx
+++ src/pages/CoinDetailsPage.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 function CoinDetailsPage()
 {
     const {coinId}=useParams();
+    // useParams is use when you want to transfer data from one page to other via url portion then use this hook which is provided by react-router-dom
     return(
         <>
             <div>Coin Details Page {coinId} </div>
