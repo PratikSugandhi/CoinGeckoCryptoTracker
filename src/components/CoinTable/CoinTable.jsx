c09b568cd648617a506e47f520b34ae73e90e5be
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-02-13 15:15:34 +0530

    Added the logic for lazy loading in image

--- src/components/CoinTable/CoinTable.jsx
+++ src/components/CoinTable/CoinTable.jsx
@@ -61,7 +61,7 @@ function CoinTable()
                             <div className="flex items-center justify-start gap-3 basis-[35%]">
 
                                 <div className="w-[5rem] h-[5rem]">
-                                    <img src={coin.image} className="w-full h-full" />
+                                    <img src={coin.image} loading="lazy"  className="w-full h-full" />
                                 </div>
 
                                 <div className="flex flex-col"> 
